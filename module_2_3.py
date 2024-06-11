aefa8a4b45ebe19f7efa265adbfaf5292943d9fa
VorobyevRoman <nag-rom@yandex.ru>
2024-06-11 19:22:54 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,9 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+a = 0
+while a < len(my_list):
+    print(my_list[a])
+    a = a + 1
+    if my_list[a] > 0:
+        continue
+    if my_list[a] < 0:
+        break
